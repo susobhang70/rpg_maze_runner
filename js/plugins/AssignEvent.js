67fa196a753fc1df3346ecf896d8ca532fb248ff
Susobhan Ghosh <susobhang70@gmail.com>
2017-11-09 21:04:43 +0530

    Merging new levels and transitionLevel commit

--- js/plugins/AssignEvent.js
+++ js/plugins/AssignEvent.js
@@ -15,9 +15,9 @@
  * @desc In-Game Region ID denoting the player respawn points after getting a wrong answer
  * @default 10
  *
- *@param Map Id Variable
- *@desc In-Game mapId
- *@default 5
+ * @param Map Id Variable
+ * @desc In-Game mapId
+ * @default 5
 */
 
 function shuffle(a) {
@@ -138,10 +138,12 @@ respawnVar = Number(PluginManager.parameters('AssignEvent')["Player Respawn Vari
 transmapid = Number(PluginManager.parameters('AssignEvent')["Map Id Variable"]);
 
 var questionPos = 0;
+var nCorrect = 0;
 var currentLevel = 0;
+var currentFinalQuestion = 0;
 var zoomIncrement = 40;
 var start = [8,7];
-var mapId = [1,3];
+var mapId = [6,7,8,1,3];
 
 createEvent = function() {
 
@@ -163,13 +165,20 @@ createEvent = function() {
 	$gameMessage.setChoiceCallback(function(responseIndex) {
 		if(responseIndex === answerIndex)
 		{
-			questionPos++;
 			setTimeout(function() {
+				questionPos++;
+				nCorrect++;
+				if(questionPos == questions.length)
+					questionPos = 0;
 				$gameVariables.setValue(responseVar, 1);
 				$gameMessage.add("Yes! Seems I am correct, now I can see properly.");
 				$gameVariables.setValue(zoomVar, currentZoom + zoomIncrement);
 				$gamePlayer.setMoveSpeed($gamePlayer.moveSpeed() + 0.5);
 				$gamePlayer._animationId = 41;
+
+				// spawn end events after 4 correct answers only
+				// if((nCorrect) % 4 == 0)
+					// Galv.SPAWN.event(2, 200);
 			}, 1000);
 		}
 		else
@@ -184,15 +193,12 @@ createEvent = function() {
 			}, 1000);
 		}
 	});
-
-	if(questionPos === questions.length)
-		questionPos = 0;
 }
 
 createFinalLevelEvent = function() {
 
-	var currentQuestion = finalQuestions[currentLevel][0];
-	var options = finalQuestions[currentLevel].slice(1, finalQuestions[currentLevel].length);
+	var currentQuestion = finalQuestions[currentFinalQuestion][0];
+	var options = finalQuestions[currentFinalQuestion].slice(1, finalQuestions[currentFinalQuestion].length);
 	var answer = options[0];
 	shuffle(options);
 	var answerIndex = options.indexOf(answer);
@@ -215,8 +221,9 @@ createFinalLevelEvent = function() {
 				$gamePlayer._animationId = 41;
 
 				currentLevel++;
-				if(currentLevel === finalQuestions.length)
-					currentLevel = 0;
+				currentFinalQuestion++;
+				if(currentFinalQuestion == finalQuestions.length)
+					currentFinalQuestion = 0;
 
 				// $gamePlayer.reserveTransfer(mapId[currentLevel], start[0], start[1], 0, 1);
 				$gamePlayer.reserveTransfer(transmapid, 16, 12, 0, 1);
